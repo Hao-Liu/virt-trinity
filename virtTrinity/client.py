e40c4444667fd490670e2cffb181dabbcbdbec5f
Hao Liu <hliu@redhat.com>
2015-06-02 17:25:35 +0800

    client: Implement client curses result presentation
    Signed-off-by: Hao Liu <hliu@redhat.com>

--- virtTrinity/client.py
+++ virtTrinity/client.py
@@ -1,7 +1,9 @@
 import re
 import os
 import sys
+import time
 import json
+import curses
 import random
 import logging
 import requests
@@ -9,6 +11,8 @@ import argparse
 import threading
 
 from collections import Counter
+from xml.dom import minidom
+from xml.etree import ElementTree
 
 from virtTrinity import provider
 from virtTrinity.utils import rnd
@@ -58,7 +62,38 @@ class VirtTrinityApp(object):
         )
         self.args, _ = self.parser.parse_known_args()
 
-    def _stat_result(self, item, counters, err_stat):
+        self.providers = self._process_providers()
+
+        self.stats = {
+            "skip": Counter(),
+            "failure": Counter(),
+            "success": Counter(),
+            "timeout": Counter(),
+            "expected_neg": Counter(),
+            "unexpected_neg": Counter(),
+            "unexpected_pass": Counter(),
+        }
+        self.counters = {
+            "cnt": 0,
+            "skip": 0,
+            "failure": 0,
+            "success": 0,
+            "timeout": 0,
+            "expected_neg": 0,
+            "unexpected_neg": 0,
+            "unexpected_pass": 0,
+        }
+        self.exiting = False
+        self.pause = False
+        self.test_thread = threading.Thread(
+            target=self.run_tests,
+        )
+        self.send_queue = []
+        self.last_item = None
+        self.cur_counter = 'failure'
+        self.screen = curses.initscr()
+
+    def _stat_result(self, item):
         """
         Categorizes and keep the count of a result of a test item depends on
         the expected failure patterns.
@@ -67,80 +102,116 @@ class VirtTrinityApp(object):
         fail_patts = item.fail_patts
 
         test_pattern = 'PATTERNS.TO.TEST.ON'
+
+        self.counters['cnt'] += 1
         if res:
             if res.exit_status == 'timeout':
-                counters['timeout_cnt'] += 1
-                print 'TIMEOUT:'
-                res.pprint()
+                self.counters['timeout'] += 1
 
             if fail_patts:
                 if res.exit_status == 'success':
-                    counters['unexpected_pass_cnt'] += 1
-                    print 'UNEXPECTED PASS: (Expect %s)' % fail_patts
-                    res.pprint()
+                    self.counters['unexpected_pass'] += 1
+                    self.stats['unexpected_pass'][res.stderr] += 1
                 elif res.exit_status == 'failure':
                     if any([re.search(p, res.stderr) for p in fail_patts]):
-                        counters['expected_neg_cnt'] += 1
+                        self.counters['expected_neg'] += 1
+                        self.stats['expected_neg'][res.stderr] += 1
                     else:
-                        counters['unexpected_neg_cnt'] += 1
-                        print 'UNEXPECTED FAIL: (Expect %s)' % fail_patts
-                        res.pprint()
+                        self.counters['unexpected_neg'] += 1
+                        self.stats['unexpected_neg'][res.stderr] += 1
             else:
                 if res.exit_status == 'success':
-                    counters['success_cnt'] += 1
+                    self.counters['success'] += 1
+                    self.stats['success'][res.stderr] += 1
                 elif res.exit_status == 'failure':
-                    err_stat[res.stderr] += 1
-                    counters['failure_cnt'] += 1
+                    self.stats['failure'][res.stderr] += 1
+                    self.counters['failure'] += 1
                     if test_pattern in res.stderr:
                         item.pprint()
                         exit(0)
         else:
-            counters['skip_cnt'] += 1
+            self.counters['skip'] += 1
 
-    def _report(self, counters, err_stat):
+    def _show_report(self):
         """
-        Print a simple statistics on results depends on the expected failure
-        patterns.
+        Show the most frequent pattern of a specified result type depends on
+        the expected failure patterns.
         """
-        cnt = counters["cnt"]
-        skip_cnt = counters["skip_cnt"]
-        failure_cnt = counters["failure_cnt"]
-        success_cnt = counters["success_cnt"]
-        timeout_cnt = counters["timeout_cnt"]
-        expected_neg_cnt = counters["expected_neg_cnt"]
-        unexpected_neg_cnt = counters["unexpected_neg_cnt"]
-        unexpected_pass_cnt = counters["unexpected_pass_cnt"]
-        print '\r' + "STATISTICS".center(80, '_')
-        print "Success  : %.2f%% (%d/%d)" % (
-            float(success_cnt) / cnt * 100,
-            success_cnt, cnt)
-        print "Failure  : %.2f%% (%d/%d)" % (
-            float(failure_cnt) / cnt * 100,
-            failure_cnt, cnt)
-        print "Skipped  : %.2f%% (%d/%d)" % (
-            float(skip_cnt) / cnt * 100,
-            skip_cnt, cnt)
-        print "ExpFail  : %.2f%% (%d/%d)" % (
-            float(expected_neg_cnt) / cnt * 100,
-            expected_neg_cnt, cnt)
-        print "UnexpFail: %.2f%% (%d/%d)" % (
-            float(unexpected_neg_cnt) / cnt * 100,
-            unexpected_neg_cnt, cnt)
-        print "UnexpPass: %.2f%% (%d/%d)" % (
-            float(unexpected_pass_cnt) / cnt * 100,
-            unexpected_pass_cnt, cnt)
-        print "Timeout  : %.2f%% (%d/%d)" % (
-            float(timeout_cnt) / cnt * 100,
-            timeout_cnt, cnt)
-        print "FREQUENT FAILS".center(80, '_')
-        print "%6s%6s%68s" % ('COUNT', 'RATIO', 'STDERR'.center(68))
-        for err, c in err_stat.most_common(30):
-            print "%5d %6.2f" % (c, float(c) * 100 / failure_cnt),
-            for idx, l in enumerate(err.splitlines()):
-                if idx == 0:
-                    print "\t" + l
+        maxy, maxx = self.screen.getmaxyx()
+        width, height = maxx / 2, maxy
+
+        cnt = self.counters["cnt"]
+
+        cur_y = 1
+        item_id = 0
+        pad = curses.newpad(height, width)
+        stat_cnt = self.counters[self.cur_counter]
+        for err, c in self.stats[self.cur_counter].most_common(30):
+            pad.addstr(cur_y, 1, str(c))
+            pad.addstr(cur_y, 6, "%6.2f" % (float(c) * 100 / stat_cnt))
+
+            style = curses.A_BOLD if bool(item_id % 2) else curses.A_DIM
+            for line in err.splitlines():
+                if len(line) > (width - 17):
+                    line = line[:(width - 17)]
+                pad.addstr(cur_y, 15, line, style)
+                cur_y += 1
+                if cur_y == height:
+                    break
+            if cur_y == height:
+                break
+            item_id += 1
+        pad.box()
+        title = self.cur_counter.replace('_', ' ').upper()
+        pad.addstr(0, 10, ' MOST FREQUENT %s ' % title)
+        if cnt:
+            pad.addstr(0, 2, '%.2f%%' % (float(stat_cnt) / cnt * 100))
+        pad.refresh(0, 0, 0, 0, height, width)
+
+    def _show_last_result(self):
+        """
+        Show last result of tested items
+        """
+        maxy, maxx = self.screen.getmaxyx()
+        width, height = maxx / 2, maxy
+
+        pad = curses.newpad(height, width)
+        item = self.last_item
+        if item:
+            lines = ''
+            lines = str(item.res)
+            if item.xml is not None:
+                if isinstance(item.xml, ElementTree.Element):
+                    xml = minidom.parseString(
+                        ElementTree.tostring(item.xml)).toprettyxml()
                 else:
-                    print "\t\t" + l
+                    xml = str(item.xml)
+                lines += xml
+            cur_y = 1
+            for line in lines.splitlines():
+                line = line.replace('\t', '  ')
+                if len(line) > (width - 2):
+                    line = line[:(width - 2)]
+                pad.addstr(cur_y, 1, line)
+                cur_y += 1
+                if cur_y == height:
+                    break
+        pad.box()
+        pad.addstr(0, 10, ' LAST RESULT ')
+        pad.refresh(0, 0, 0, maxx / 2, maxy, maxx)
+
+    def _show_exit(self):
+        """
+        Show existing pad in the center of screen
+        """
+        maxy, maxx = self.screen.getmaxyx()
+        width, height = 30, 5
+
+        pad = curses.newpad(height, width)
+        pad.box()
+        pad.addstr(2, 10, ' EXISTING... ')
+        pad.refresh(0, 0, maxy / 2 - 3, maxx / 2 - 15,
+                    maxy / 2 + 3, maxx / 2 + 15)
 
     def _process_providers(self):
         """
@@ -184,62 +255,83 @@ class VirtTrinityApp(object):
                 auth=(self.args.username, self.args.password),
             )
             if response.status_code != 201:
-                logging.error('Failed to send result (HTTP%s):',
+                logging.debug('Failed to send result (HTTP%s):',
                               response.status_code)
                 if 'DOCTYPE' in response.text:
                     html_path = 'debug_%s.html' % rnd.regex('[a-z]{4}')
                     with open(html_path, 'w') as fp:
                         fp.write(response.text)
-                    logging.error('Html response saved to %s',
+                    logging.debug('Html response saved to %s',
                                   os.path.abspath(html_path))
                 else:
-                    logging.error(response.text)
+                    logging.debug(response.text)
         except requests.ConnectionError, detail:
-            logging.error('Failed to send result to server: %s', detail)
+            logging.debug('Failed to send result to server: %s', detail)
+
+    def run_tests(self):
+        while not self.exiting:
+            item = random.choice(self.providers.values()).run_once()
+            self.last_item = item
+            self.send_queue.append(item)
+            self._stat_result(item)
+            if self.pause:
+                while self.pause and not self.exiting:
+                    time.sleep(0.5)
 
     def run(self):
         """
         Main loop to run tests and send/report tests results.
         """
         os.environ["EDITOR"] = "echo"
-        providers = self._process_providers()
-
-        err_stat = Counter()
-        counters = {
-            "cnt": 0,
-            "skip_cnt": 0,
-            "failure_cnt": 0,
-            "success_cnt": 0,
-            "timeout_cnt": 0,
-            "expected_neg_cnt": 0,
-            "unexpected_neg_cnt": 0,
-            "unexpected_pass_cnt": 0,
-        }
-        send_queue = []
         last_thread = None
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(0)
+        self.screen.keypad(1)
+        self.screen.timeout(100)
+        self.screen.refresh()
+
+        self.last_item = None
+        self.test_thread.start()
+
         try:
             while True:
-                item = random.choice(providers.values()).run_once()
-                send_queue.append(item)
+                self._show_report()
+                self._show_last_result()
+                if last_thread:
+                    last_thread.join()
 
-                self._stat_result(item, counters, err_stat)
+                # Monitor key press
+                ch = self.screen.getch()
+                if ch == ord('q'):
+                    break
+                elif ch == ord('p'):
+                    self.pause = not self.pause
+                elif ch == ord('n'):
+                    idx = self.stats.keys().index(self.cur_counter)
+                    new_idx = (idx + 1) % len(self.stats)
+                    self.cur_counter = self.stats.keys()[new_idx]
+                elif ch == ord('N'):
+                    idx = self.stats.keys().index(self.cur_counter)
+                    new_idx = (idx - 1) % len(self.stats)
+                    self.cur_counter = self.stats.keys()[new_idx]
 
-                counters['cnt'] += 1
-                cnt = counters['cnt']
-                if cnt % 200 == 0 and cnt != 0:
-                    self._report(counters, err_stat)
-                    if last_thread:
-                        last_thread.join()
+                if len(self.send_queue) > 200:
                     send_thread = threading.Thread(
                         target=self.send,
-                        args=(send_queue,)
+                        args=(self.send_queue,)
                     )
                     send_thread.start()
                     last_thread = send_thread
-                    send_queue = []
-                else:
-                    sys.stdout.write('\r%s\r' % cnt)
-                    sys.stdout.flush()
+                    self.send_queue = []
         except KeyboardInterrupt:
-            self._report(counters, err_stat)
-            sys.exit(0)
+            pass
+        finally:
+            self.exiting = True
+            self._show_exit()
+            self.test_thread.join()
+            curses.nocbreak()
+            self.screen.keypad(0)
+            curses.curs_set(1)
+            curses.echo()
+            curses.endwin()
